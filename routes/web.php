75385190d8e2f98cf3bbf407ae9f4d04f0a600f3
anilkumarjaiswal25 <jaiswalanilkumar462@gmail.com>
2026-01-30 23:37:38 +0530

    Added migration route and fixed CORS

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/init-db', function () {
+    try {
+        Artisan::call('migrate:fresh', ['--force' => true]);
+        return "Database Migrated Successfully!";
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
 });
